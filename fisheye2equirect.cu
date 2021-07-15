0f39782bac0dcbb2d9b9ccb78014a1362d4371cd
ShinYwings <ski8992@gmail.com>
2021-07-15 17:37:01 +0900

    cuda implementation

---
+++ fisheye2equirect.cu
@@ -0,0 +1,325 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/cudaimgproc.hpp>
+#include <chrono>
+#include <vector>
+#include <thread>
+#include <malloc.h>
+#include "mlut.h"
+#include "cuda_runtime.h"
+
+using namespace std;
+using namespace cv;
+
+// test with CPU
+typedef struct{
+    int x;
+    int y;
+}int2_host;
+typedef struct{
+    float x;
+    float y;
+    float z;
+    float w;
+}float4_host;
+typedef struct{
+    unsigned char x;
+    unsigned char y;
+    unsigned char z;
+}uchar3_host;
+
+uchar3_host* upload_image_on_CPU(cv::Mat image);
+
+uchar3* upload_image_on_GPU(cv::Mat image);
+
+// test on CPU
+void equisolid2Equirect_CPU(uchar3_host* img_host, int lutsize_host, int2_host* xy_host, int2_host* txty_host, float4_host* coefs_host, uchar3_host* equirect_host);
+
+__global__ void equisolid2Equirect(uchar3* img_dev, int lutsize_dev, int2* xy_dev, int2* txty_dev, float4* coefs_dev, uchar3* equirect_dev);
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
+	cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeFourByte);
+    cv::cuda::printShortCudaDeviceInfo(cv::cuda::getDevice());
+    int cuda_devices_number = cv::cuda::getCudaEnabledDeviceCount();
+    cout << "CUDA Device(s) Number: "<< cuda_devices_number << endl;
+    cv::cuda::DeviceInfo _deviceInfo;
+    bool _is_device_compatible = _deviceInfo.isCompatible();
+    cout << "CUDA Device(s) Compatible: " << _is_device_compatible << endl;
+
+    // Find Maximum thread number
+    const int threadnum = std::thread::hardware_concurrency();
+    std::cout << "thread number : " << threadnum << std::endl;
+    
+    const int equirect_w = 1024;
+    const int equirect_h = 512;
+    cv::Size newsize(equirect_w, equirect_h);
+
+    // Load lookup table
+    mlut::mLUT lutt;
+    mlut::load(lutt, "lut.txt");
+    std::vector<mlut::mappingData*> mapData = lutt.getMaps();
+    if(mapData.empty()) std::cout<<"ABORT! empty vector"<<std::endl;
+    int lutsize= static_cast<int>(mapData.size());
+    
+    ////////////////////////////////////////////////////////////
+    // CPU version
+    {
+        // Define input, output image size
+        cv::Mat srcimg = cv::imread("ex2.JPG");
+        cv::Size viewSize = srcimg.size();
+        cv::Mat equirect_host = cv::Mat::zeros(newsize, srcimg.type());
+
+        int2_host* xy_host = new int2_host[lutsize];
+        int2_host* txty_host = new int2_host[lutsize];
+        float4_host* coefs_host = new float4_host[lutsize];
+
+        for(int i=0; i<lutsize; i++)
+        {
+            mlut::mappingData map = *mapData[i];
+            mlut::xy xy_ = map.getXY();
+            mlut::txty txty_ = map.getTXTY();
+            mlut::ipCoefs coefs_ = map.getIpCoefs();
+
+            xy_host[i].x = xy_.getX();
+            xy_host[i].y = xy_.getY();
+
+            txty_host[i].x = txty_.getTX();
+            txty_host[i].y = txty_.getTY();
+
+            coefs_host[i].x = coefs_.getBL();
+            coefs_host[i].y = coefs_.getTL();
+            coefs_host[i].z = coefs_.getBR();
+            coefs_host[i].w = coefs_.getTR();
+        }
+        uchar3_host* h_fish = upload_image_on_CPU(srcimg);
+        uchar3_host* h_equirect = upload_image_on_CPU(equirect_host);
+        
+        // Multi-threading
+        // std::vector<std::vector<mlut::mappingData*>> pool;
+        // vector<thread> workers;
+        // int poolsize = static_cast<int>(ceil(mapData.size() / threadnum));
+        // auto offset = mapData.cbegin();
+        // for(int i=0; i<threadnum; i++)
+        // {
+        //     std::vector<mlut::mappingData*> map;
+        //     auto tmp = offset+poolsize;
+
+        //     if (tmp >= mapData.cend()) tmp = mapData.cend()-1;
+        //     map.assign(offset, tmp);
+        //     pool.emplace_back(map);
+        //     offset = tmp;
+        // }
+
+        // for (int i = 0; i < threadnum; i++) {
+        //     workers.emplace_back(std::thread(equisolid2Equirect, std::ref(srcimg), std::ref(pool[i]), std::ref(equirect)));
+        // }
+        // for (int i = 0; i < threadnum; i++) {
+        //     workers[i].join();
+        // }
+
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        equisolid2Equirect_CPU(h_fish, lutsize, xy_host, txty_host, coefs_host, h_equirect);
+        memcpy(equirect_host.ptr(), h_equirect, equirect_h*equirect_w*sizeof(uchar3_host));
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();	
+        std::cout << "[HOST] elapsed time : " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << std::endl;
+
+        imshow("host equisolid2panorama", equirect_host);
+        waitKey(0);
+    }
+
+    ////////////////////////////////////////////////////////
+    //// GPU version
+    int2* xy = new int2[lutsize];
+    int2* txty = new int2[lutsize];
+    float4* coefs = new float4[lutsize];
+
+    for(int i=0; i<lutsize; i++)
+    {
+        mlut::mappingData map = *mapData[i];
+        mlut::xy xy_ = map.getXY();
+        mlut::txty txty_ = map.getTXTY();
+        mlut::ipCoefs coefs_ = map.getIpCoefs();
+
+        xy[i].x = xy_.getX();
+        xy[i].y = xy_.getY();
+
+        txty[i].x = txty_.getTX();
+        txty[i].y = txty_.getTY();
+
+        coefs[i].x = coefs_.getBL();
+        coefs[i].y = coefs_.getTL();
+        coefs[i].z = coefs_.getBR();
+        coefs[i].w = coefs_.getTR();
+    }
+
+    int threadsize = 512; // Maximum thread size on a block
+    int blocksize = (lutsize+threadsize-1)/threadsize;
+
+    float elapsedTime = 0;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    {
+        // Define input, output image size
+        cv::Mat srcimg = cv::imread("ex2.JPG");
+        cv::Mat equirect = cv::Mat::zeros(newsize, srcimg.type());
+
+        uchar3* d_fish = upload_image_on_GPU(srcimg);
+        uchar3* d_equirect = upload_image_on_GPU(equirect);
+
+        int2* d_xy;
+        int2* d_txty;
+        float4* d_coefs;
+        
+        cudaMalloc(&d_xy, lutsize*sizeof(int2));
+        cudaMalloc(&d_txty, lutsize*sizeof(int2));
+        cudaMalloc(&d_coefs, lutsize*sizeof(float4));
+
+        cudaMemcpy(d_xy, xy, lutsize*sizeof(int2), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_txty, txty, lutsize*sizeof(int2), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_coefs, coefs, lutsize*sizeof(float4), cudaMemcpyHostToDevice);
+
+        cudaEventRecord(start, 0);
+        equisolid2Equirect<<<blocksize,threadsize>>>(d_fish, lutsize, d_xy, d_txty, d_coefs, d_equirect);
+        cudaMemcpy(equirect.ptr(), d_equirect, equirect_h*equirect_w*sizeof(uchar3), cudaMemcpyDeviceToHost);
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+
+        cudaEventElapsedTime(&elapsedTime, start, stop);
+
+        std::cout << "[DEVICE] GPU operation elapsed time : " << elapsedTime << "ms" << std::endl;
+
+        // imshow("equisolid2panorama", equirect);
+        // waitKey(0);
+        
+        // imwrite("sample_result.jpg", equirect_res);
+
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+        cudaFree(d_fish);
+        cudaFree(d_equirect);
+        cudaFree(d_xy);
+        cudaFree(d_txty);
+        cudaFree(d_coefs);
+    }
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();	
+    std::cout << "[DEVICE] the elapsed time of the whole process: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << std::endl;
+
+    delete xy;
+    delete txty;
+    delete coefs;
+
+    return 0;
+}
+
+uchar3_host* upload_image_on_CPU(cv::Mat image)
+{
+    uchar3_host *h_image_host = new uchar3_host[image.rows * image.cols];
+    unsigned char *cvPtr_host = image.ptr<unsigned char>(0);
+
+    for(int i =0; i< image.rows * image.cols; ++i)
+    {
+        (h_image_host)[i].x = cvPtr_host[3*i+0];
+        (h_image_host)[i].y = cvPtr_host[3*i+1];
+        (h_image_host)[i].z = cvPtr_host[3*i+2];
+    }
+    return h_image_host;
+}
+
+uchar3* upload_image_on_GPU(cv::Mat image)
+{
+    uchar3 *h_image = new uchar3[image.rows * image.cols];
+    uchar3 *d_in;
+    unsigned char *cvPtr = image.ptr<unsigned char>(0);
+
+    for(int i =0; i< image.rows * image.cols; ++i)
+    {
+        (h_image)[i].x = cvPtr[3*i+0];
+        (h_image)[i].y = cvPtr[3*i+1];
+        (h_image)[i].z = cvPtr[3*i+2];
+    }
+
+    size_t numRows = image.rows;
+    size_t numCols = image.cols;
+
+    cudaMalloc((void **) &d_in, numRows*numCols* sizeof(uchar3));
+    cudaMemcpy(d_in, h_image, numRows * numCols * sizeof(uchar3), cudaMemcpyHostToDevice);
+
+    free(h_image);
+
+    return d_in;
+}
+
+void equisolid2Equirect_CPU(uchar3_host* img_host, int lutsize_host, int2_host* xy_host, int2_host* txty_host, float4_host* coefs_host, uchar3_host* equirect_host)
+{
+    for(int x_host =0; x_host < lutsize_host; x_host++)
+    {
+        unsigned long curr_pos_host = txty_host[x_host].y*1600+txty_host[x_host].x;
+
+        // if(x_host < 55000 && 54000<x_host)
+        //     printf("[HOST] tx, ty : %d, %d and offset : %d \n", txty_host[x_host].x, txty_host[x_host].y, x_host);
+
+        float bl_b_host = img_host[curr_pos_host].x * coefs_host[x_host].x;
+        float bl_g_host = img_host[curr_pos_host].y * coefs_host[x_host].x;
+        float bl_r_host = img_host[curr_pos_host].z * coefs_host[x_host].x;
+        
+        float tl_b_host = img_host[curr_pos_host+1600].x * coefs_host[x_host].y;
+        float tl_g_host = img_host[curr_pos_host+1600].y * coefs_host[x_host].y;
+        float tl_r_host = img_host[curr_pos_host+1600].z * coefs_host[x_host].y;
+
+        float br_b_host = img_host[curr_pos_host+1].x * coefs_host[x_host].z;
+        float br_g_host = img_host[curr_pos_host+1].y * coefs_host[x_host].z;
+        float br_r_host = img_host[curr_pos_host+1].z * coefs_host[x_host].z;
+        
+        float tr_b_host = img_host[curr_pos_host+1601].x * coefs_host[x_host].w;
+        float tr_g_host = img_host[curr_pos_host+1601].y * coefs_host[x_host].w;
+        float tr_r_host = img_host[curr_pos_host+1601].z * coefs_host[x_host].w;
+
+        equirect_host[xy_host[x_host].y*1024+xy_host[x_host].x].x = bl_b_host + tl_b_host + br_b_host + tr_b_host;
+        equirect_host[xy_host[x_host].y*1024+xy_host[x_host].x].y = bl_g_host + tl_g_host + br_g_host + tr_g_host;
+        equirect_host[xy_host[x_host].y*1024+xy_host[x_host].x].z = bl_r_host + tl_r_host + br_r_host + tr_r_host;
+    } 
+}
+
+__global__ void equisolid2Equirect(uchar3* img_dev, int lutsize_dev, int2* xy_dev, int2* txty_dev, float4* coefs_dev, uchar3* equirect_dev)
+{
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
+    int offset = x + y * blockDim.x * gridDim.x;
+    
+    // img size 1600, 1600
+    // equirect 1024, 512
+
+    if (offset < lutsize_dev)
+    {
+        unsigned long curr_pos = txty_dev[offset].y*1600+txty_dev[offset].x;
+
+        float bl_b = img_dev[curr_pos].x * coefs_dev[offset].x;
+        float bl_g = img_dev[curr_pos].y * coefs_dev[offset].x;
+        float bl_r = img_dev[curr_pos].z * coefs_dev[offset].x;
+        
+        float tl_b = img_dev[curr_pos+1600].x * coefs_dev[offset].y;
+        float tl_g = img_dev[curr_pos+1600].y * coefs_dev[offset].y;
+        float tl_r = img_dev[curr_pos+1600].z * coefs_dev[offset].y;
+
+        float br_b = img_dev[curr_pos+1].x * coefs_dev[offset].z;
+        float br_g = img_dev[curr_pos+1].y * coefs_dev[offset].z;
+        float br_r = img_dev[curr_pos+1].z * coefs_dev[offset].z;
+        
+        float tr_b = img_dev[curr_pos+1601].x * coefs_dev[offset].w;
+        float tr_g = img_dev[curr_pos+1601].y * coefs_dev[offset].w;
+        float tr_r = img_dev[curr_pos+1601].z * coefs_dev[offset].w;
+
+        equirect_dev[xy_dev[offset].y*1024+xy_dev[offset].x].x = bl_b + tl_b + br_b + tr_b;
+        equirect_dev[xy_dev[offset].y*1024+xy_dev[offset].x].y = bl_g + tl_g + br_g + tr_g;
+        equirect_dev[xy_dev[offset].y*1024+xy_dev[offset].x].z = bl_r + tl_r + br_r + tr_r;
+        }  
+}
---
+++ fisheye_cali.cpp
@@ -0,0 +1,336 @@
+// fisheye correction refers from
+// https://github.com/astar-ai/calicam_mono/blob/master/calicam_mono.cpp
+//proportional to latitude.  (equisolid -> equidistant)
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <regex>
+#include <dirent.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/calib3d.hpp>
+#include <string>
+#include <chrono>
+#include <cmath>
+
+const int BOARDWIDTH = 7;
+const int BOARDHEIGHT = 10;
+
+float SQUARESIZE = 25;
+
+using namespace std;
+using namespace cv;
+
+vector<string> viewlistname;
+
+inline double MatRowMul(cv::Matx33d m, double x, double y, double z, int r) {
+  return m(r,0) * x + m(r,1) * y + m(r,2) * z;
+}
+
+inline double lerp(double y0, double y1, double x0, double x1, double x)
+{
+    double m = (y1-y0) / (x1-x0);
+    double b = y0;
+    return m * (x-x0) + b;
+}
+
+class CalibSettings
+{
+    public:
+    int getFlag()
+    {
+        int flag = 0;
+        flag |= cv::fisheye::CALIB_RECOMPUTE_EXTRINSIC;
+        flag |= cv::fisheye::CALIB_CHECK_COND;
+        flag |= cv::fisheye::CALIB_FIX_SKEW;
+        return flag;
+    }
+
+    Size getBoardSize()
+    {
+        return Size(BOARDWIDTH, BOARDHEIGHT);
+    }
+
+    float getSquareSize()
+    {
+        return SQUARESIZE;
+    }
+};
+
+CalibSettings s;
+static void calcBoardCornerPositions(Size boardSize, float squareSize, vector<Point3f>& corners)
+{
+    corners.clear();
+    for (int i = 0; i < boardSize.height; ++i)
+        for (int j = 0; j < boardSize.width; ++j)
+            corners.push_back(Point3f(j * squareSize, i * squareSize, 0));
+}
+
+vector<string> getImageList(string path)
+{
+    vector<string> imagesName;
+    DIR* dir;
+    struct dirent* ent;
+    if ((dir = opendir(path.c_str())) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            string tmpFileName = ent->d_name;
+            if (tmpFileName.length() > 4) {
+                auto nPos = tmpFileName.find(".jpg");
+                if (nPos != string::npos) {
+                    imagesName.push_back(path + '/' + tmpFileName);
+                    viewlistname.push_back(tmpFileName);
+                } else {
+                    nPos = tmpFileName.find(".JPG");
+                    if (nPos != string::npos)
+                        imagesName.push_back(path + '/' + tmpFileName);
+                        viewlistname.push_back(tmpFileName);
+                }
+            }
+        }
+        closedir(dir);
+    }
+    return imagesName;
+}
+
+cv::Vec2d rad2vec(double r, double theta, double phi, Vec2d f, Vec2d c, Vec4d kp)
+{
+    double fx = f[0], fy = f[1];
+    double cx = c[0], cy = c[1];
+    
+    double theta2 = theta*theta, theta3 = theta2*theta, theta4 = theta2*theta2, theta5 = theta4*theta,
+        theta6 = theta3*theta3, theta7 = theta6*theta, theta8 = theta4*theta4, theta9 = theta8*theta;
+
+    double theta_d = theta + kp[0]*theta3 + kp[1]*theta5 + kp[2]*theta7 + kp[3]*theta9;
+
+    double inv_r = r > 1e-8 ? 1.0/r : 1;
+    double cdist = r > 1e-8 ? theta_d * inv_r : 1;
+
+    double x_src_norm = r * cos(phi);
+    double y_src_norm = r * sin(phi);
+
+    double x_src = cdist * x_src_norm;
+    double y_src = cdist * y_src_norm;
+
+    return cv::Vec2d(fx*x_src+cx, fy*y_src+cy);
+} 
+
+void fisheye2Equirect(InputArray img, InputArray K, InputArray D, const cv::Size& newsize, const double& aperture, Mat& equirect)
+{
+    CV_Assert((K.depth() == CV_32F || K.depth() == CV_64F) && (D.depth() == CV_32F || D.depth() == CV_64F));
+    CV_Assert(K.size() == Size(3, 3) && (D.empty() || D.total() == 4));
+
+    cv::Vec2d f, c;
+    if (K.depth() == CV_32F)
+    {
+        Matx33f camMat = K.getMat();
+        f = Vec2f(camMat(0, 0), camMat(1, 1));
+        c = Vec2f(camMat(0, 2), camMat(1, 2));
+    }
+    else
+    {
+        Matx33d camMat = K.getMat();
+        f = Vec2d(camMat(0, 0), camMat(1, 1));
+        c = Vec2d(camMat(0, 2), camMat(1, 2));
+    }
+
+    Mat img_ = img.getMat();
+    Size viewSize = img_.size();
+
+    Vec4d kp = Vec4d::all(0);
+    if (!D.empty())
+        kp = D.depth() == CV_32F ? (Vec4d)*D.getMat().ptr<Vec4f>(): *D.getMat().ptr<Vec4d>();
+
+    for(int y=0; y< newsize.height; y++)
+    {
+        double y_dst_norm = lerp(-1,1,0,newsize.height, y);
+
+        for(int x=0; x< newsize.width; x++)
+        {
+            double x_dst_norm = lerp(-1,1,0, newsize.width, x);
+            
+            double longitude = x_dst_norm * CV_PI + CV_PI / 2;
+            double latitude = y_dst_norm * CV_PI / 2;
+
+            double p_x = - cos(latitude) * cos(longitude);
+            double p_y = cos(latitude) * sin(longitude);
+            double p_z = sin(latitude);
+    
+            Vec3d p = Vec3d(p_x, p_y, p_z);
+            cv::Mat rot = (cv::Mat_<double>(3,3) << 1, 0, 0, 0, 0, -1, 0, 1, 0);
+            cv::Mat pt = rot * p;
+            double p_xz = sqrt(pow(pt.at<double>(0,0), 2)+pow(pt.at<double>(0,2), 2));
+            double theta = atan2(p_xz,pt.at<double>(0,1));
+            double r = ((2 * theta) / aperture);
+            double phi = atan2(pt.at<double>(0,2), pt.at<double>(0,0));
+
+            Vec2d uv = rad2vec(r, theta, phi, f, c, kp);
+
+            if( y > newsize.height / 2 -1 )
+            {
+                continue;
+            }
+            else if(y == 255)
+            {
+                equirect.at<Vec3b>(y,x) = Vec3b(255, 0., 0.);
+            }
+            else
+            {
+                double tx = min(viewSize.width - 1, static_cast<int>(floor(uv[0])));
+                double ty = min(viewSize.height - 1, static_cast<int>(floor(uv[1])));
+
+                double a = uv[0] - tx;
+                double b = uv[1] - ty;
+
+                if(tx >= 0 && tx < viewSize.width -1 && ty >=0 && ty < viewSize.height -1)
+                {
+                    if(tx == viewSize.width -1) tx-=1;
+                    if(ty == viewSize.height -1) ty-=1;
+
+                    Vec3d c_top = img_.at<Vec3b>(ty+1,tx) * (1. - a) + img_.at<Vec3b>(ty+1,tx+1) * (a); 
+                    Vec3d c_bot = img_.at<Vec3b>(ty,tx) * (1. - a) + img_.at<Vec3b>(ty,tx+1) * (a);
+                    equirect.at<Vec3b>(y,x) = c_bot * (1. - b) + c_top * b;
+                }
+            }
+        }
+    }
+}
+
+string readImageName(string imgpath)
+{   
+    Mat view = image.getMat();
+    std::regex re("IMG_\\d{3,4}");
+    std::smatch match;
+    string imgname;
+    if(std::regex_search(imgpath, match, re))
+    {
+        for (size_t j=0; j<match.size(); j++)
+        {
+            imgname = match[j].str();
+        }
+    }
+    
+    return imgname;
+}
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    if (argc < 2) {
+        cout << "Uge: " << argv[0] << " <pic path> [square size(mm)]" << endl;
+        return 0;
+    }
+    if (argc == 3) {
+        std::string size;
+        size = argv[2];
+        SQUARESIZE = std::stof(size);
+    }
+    string pathDirectory = argv[1];
+    vector<string> imagesName = getImageList(pathDirectory);
+    
+    vector<vector<Point2f>> imagePoints;
+    Size imageSize;
+    vector<vector<Point3f>> objectPoints;
+    vector<string> viewlist;
+
+    cout << "imagename size : " << imagesName.size() << endl;
+    
+    for (vector<string>::iterator image_name= imagesName.begin(); image_name!=imagesName.end(); image_name++ ) {
+
+        string imgpath = *image_name;
+        Mat view = imread(imgpath);
+        Mat view_clone = view.clone();
+
+        imageSize = view.size();
+        vector<Point2f> pointBuf;
+        // find the corners
+        bool found = findChessboardCorners(view, s.getBoardSize(), pointBuf, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_ACCURACY |CALIB_CB_NORMALIZE_IMAGE);
+        if (found) {
+            Mat viewGray;
+            cvtColor(view, viewGray, COLOR_BGR2GRAY);
+            cornerSubPix(viewGray, pointBuf, Size(11, 11), Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::COUNT + TermCriteria::MAX_ITER, imagesName.size(), 1e-10));
+            imagePoints.push_back(pointBuf);
+            drawChessboardCorners(view, s.getBoardSize(), Mat(pointBuf), found);
+            cout << *image_name << endl;
+            viewlist.push_back(*image_name);
+
+            string name = "./original/" + readImageName(imgpath) + "_ori.jpg";
+            cout << "written in " << name << endl;
+            cv::imwrite(name, view);
+
+            vector<Point3f> obj;
+            calcBoardCornerPositions(s.getBoardSize(), s.getSquareSize(), obj);
+            objectPoints.push_back(obj);
+        } else {
+            cout << *image_name << " found corner failed! & removed!" << endl;
+            imagesName.erase(image_name);
+        }
+    }
+
+    cout << "Usable images size : " << imagesName.size() << endl;
+
+    cv::Mat cameraMatrix, xi, distCoeffs;
+
+    vector<Mat> rvec, tvec;
+    
+    Mat mIdx;
+
+    cout << "-------------imageSize--------------" << endl;
+    cout << imageSize << endl;
+
+    double rms = fisheye::calibrate(objectPoints, imagePoints, imageSize, cameraMatrix, distCoeffs, rvec, tvec, s.getFlag(), TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, objectPoints.size(), 1e-10));
+    
+    cout << "-------------mean Reprojection error--------------" << endl;
+    cout << rms << endl;
+    cout << "-------------cameraMatrix--------------" << endl;
+    cout << cameraMatrix << endl;
+    cout << "---------------distCoeffs--------------" << endl;
+    cout << distCoeffs << endl;
+    
+    vector<Mat> rep_viewlist;
+
+    int idx = 0;
+    for (auto image_name : viewlist) {
+
+        string imgpath = image_name.c_str();
+        Mat view = imread(imgpath);
+
+        imageSize = view.size();
+        vector <Point2f> rep_corner_pts;
+        
+        Mat viewGray;
+        cvtColor(view, viewGray, COLOR_BGR2GRAY);
+        
+        fisheye::projectPoints(cv::Mat(objectPoints[idx]), rep_corner_pts, rvec[idx], tvec[idx], cameraMatrix, distCoeffs);
+        
+        cornerSubPix(viewGray, rep_corner_pts, Size(11, 11), Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::COUNT + TermCriteria::MAX_ITER, imagesName.size(), 1e-10));
+
+        drawChessboardCorners(view, s.getBoardSize(), rep_corner_pts, true);
+
+        cout << imgpath << endl;
+        rep_viewlist.push_back(view);
+        imshow("view", view);
+        waitKey(0);
+
+        string name = "./reproject/" + readImageName(imgpath) + "_rep.jpg";
+        cout << "written in " << name << endl;
+        cv::imwrite(name, view);
+        idx++;
+    }
+
+    Size newsize(1024,512);
+    double aperture = 180. * CV_PI / 180.;
+    
+    Mat srcimg = cv::imread("ex2.JPG");
+    Mat equirect = cv::Mat::zeros(newsize, srcimg.type());
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now(); 
+    fisheye2Equirect(srcimg, cameraMatrix, distCoeffs, newsize, aperture, equirect);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    cout << "elapsed time : " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << endl;
+    imshow("test", equirect);
+    imwrite("equirect2.jpg", equirect);
+    waitKey(0);
+    
+    return 0;
+}
